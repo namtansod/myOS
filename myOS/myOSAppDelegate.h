3b65401116c35426f3d168d1d0f5b3cff2c34746
MMiOS PPK <namtansod@gmail.com>
2011-09-03 11:28:37 +0700

    Initial Commit

---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by JOHN MOLLOY on 3/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
